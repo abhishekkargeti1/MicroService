7dedbc483596f67b138032cfacb105466e3e5ccd
Abhishek Kargeti1 <abhishek.kargeti1@gmail.com>
2025-09-05 09:50:21 +0530

    Added API Gateway

--- QuizService/src/main/java/com/quiz/services/QuestionClient.java
+++ QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@FeignClient(url="http://localHost:8080" ,value = "Question-Client")
+//@FeignClient(url="http://localHost:8080" ,value = "Question-Client")
+@FeignClient(name = "QuestionService" )
 public interface QuestionClient {
 
     @GetMapping("/question/byQuizId/{quizId}")
